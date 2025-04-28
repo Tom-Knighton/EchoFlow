8dee65e8cf27f8ae9303bddfeb70b1ed7499ccad
Tom K <tomknighton@icloud.com>
2025-04-28 21:56:24 +0100

    Initial Commit

---
+++ EchoFlow/EchoFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  EchoFlowApp.swift
+//  EchoFlow
+//
+//  Created by Tom Knighton on 27/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct EchoFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
